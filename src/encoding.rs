16047568f44ff295755a42eaf0a8e82aa0a19946
Emmanuel Guefif <eguefif@fastmail.com>
2025-04-05 09:43:18 -0400

    Impl decoding

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,4 +1,29 @@
+use flate2::read::DeflateDecoder;
+use flate2::read::GzDecoder;
+use std::io::Read;
+
 pub enum Encoding {
     Gzip,
     Deflate,
+}
+
+pub fn uncompress(data: Vec<u8>, encoding: Encoding) -> Vec<u8> {
+    match encoding {
+        Encoding::Gzip => gzip(data),
+        Encoding::Deflate => deflate(data),
+    }
+}
+
+fn gzip(data: Vec<u8>) -> Vec<u8> {
+    let mut decoder = GzDecoder::new(&data[..]);
+    let mut retval = vec![0u8; 0];
+    let _ = decoder.read(&mut retval);
+    retval
+}
+
+fn deflate(data: Vec<u8>) -> Vec<u8> {
+    let mut decoder = DeflateDecoder::new(&data[..]);
+    let mut retval = vec![0u8; 0];
+    let _ = decoder.read(&mut retval);
+    retval
 }
